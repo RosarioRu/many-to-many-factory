e428bc3c1b43a1b39f3d09e8b12982e21a1785dd
RosarioRu <mariarruval@gmail.com>
2022-08-11 14:20:24 -0700

    Add code for Machine and Engineer models with auto-implementing properties and collection navigational property for the JoinEntities.

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -7,5 +7,16 @@ namespace Factory.Models
   public class Engineer
   {
 
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities {get; set; }
+
   }
 }
+
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,6 +6,16 @@ namespace Factory.Models
 {
   public class Machine
   {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    public string Description {get; set; }
+    public int Value {get; set}
+    public virgual ICollection<EngineerMachine> JoinEntities { get; set; }
+
 
   }
 }
