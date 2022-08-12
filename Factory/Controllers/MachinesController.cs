59712dd2096428e84e9bc8daa4aadc42a7493bf1
RosarioRu <mariarruval@gmail.com>
2022-08-11 23:33:30 -0700

    Add Create view and route in MachinesController.

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,33 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore; 
+using System.Collections.Generic;
+using System.Linq; 
+
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
+      return View();
+    }
+
+    
+  }
+}
