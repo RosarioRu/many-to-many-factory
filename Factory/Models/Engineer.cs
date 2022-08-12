7b815cd6bbcbc1e5f6b2155a9ed8173156767240
RosarioRu <mariarruval@gmail.com>
2022-08-11 22:55:44 -0700

    Add different display name for EngineerName.

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; 
 
 
 namespace Factory.Models
@@ -13,6 +14,8 @@ namespace Factory.Models
     }
 
     public int EngineerId { get; set; }
+
+    [Display(Name = "Full Name")]
     public string EngineerName { get; set; }
 
     public virtual ICollection<EngineerMachine> JoinEntities {get; set; }
