d9e2fb54002c02710cb26612be6d455b593997e1
RosarioRu <mariarruval@gmail.com>
2022-08-11 22:54:44 -0700

    Add different display name for EngineerName.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,5 +1,6 @@
 using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore; 
 using System.Collections.Generic;
 using System.Linq; 
@@ -24,7 +25,7 @@ namespace Factory.Controllers
     public ActionResult Create()
     {
       ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Description");
-      return View()
+      return View();
     }
 
 
