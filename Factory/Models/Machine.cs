7b8bbf9b3311c52df0cca91c458affba35823d81
RosarioRu <mariarruval@gmail.com>
2022-08-12 00:10:13 -0700

    Added MachineName property to Machine class.

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,12 +6,13 @@ namespace Factory.Models
 {
   public class Machine
   {
-    public Machine()
+    public Machine() 
     {
       this.JoinEntities = new HashSet<EngineerMachine>();
     }
 
     public int MachineId { get; set; }
+    public string MachineName { get; set; }
     public string Description {get; set; }
     public int Value {get; set; }
 
