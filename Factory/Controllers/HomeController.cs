5db2130d755abd8118a2c199ae2c7d6988ee9813
RosarioRu <mariarruval@gmail.com>
2022-08-12 04:04:06 -0700

    remove comments

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -5,11 +5,10 @@ namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    
     [Route("/")]
     public ActionResult Index() { return View(); }
-    // example route below:
-    // [Route("/")]
-    // public string Letter() { return "Our virtual postcard will go here soon!"; }
+    
 
   }
 }
